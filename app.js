1c3bb4b4cd0165a350ae71fb6fb180022c9a9962
Stanislav Stehniy <sstehniy@gmail.com>
2020-09-09 23:54:06 +0200

    commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static("build"));
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, err => {
+  err && console.log(err);
+});
